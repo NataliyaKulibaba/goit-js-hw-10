d1fbbf7af32e5b9657f27e2bb01ba0ffb744778e
NataliyaKulibaba <natakulibaba88@gmail.com>
2021-11-20 18:04:57 +0200

    need correct

---
+++ src/fetchCountries.js
@@ -0,0 +1,51 @@
+export default class NewsApiService {
+  constructor() {
+    this.searchCountry = '';
+    
+  }
+  fetchCountries() {
+    const BASE_URL = 'https://restcountries.com/v2';
+    return fetch(`${BASE_URL}/name/${this.searchCountry}?fields=name,capital,population,flags,languages`)
+      .then(response => {
+        return response.json();
+      });
+  }
+  
+  get name() {
+    return this.searchCountry;
+  }
+
+  set name(newName) {
+    return this.searchCountry = newName;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// export default { fetchCountries };
+
+// const BASE_URL = 'https://restcountries.com/v2';
+// // let flags = "https://restcountries.com/data/per.svg",
+
+
+
+// function fetchCountries(name) {
+
+    
+//     console.log(name)
+//   // return fetch(`${BASE_URL}/name/${name}`)
+//     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+//       //  fetch(`${BASE_URL}/name/${name}?fields ={name},{capital},{population},{languages},{flags.svg}`)
+//     .then(response => {
+//       return response.json();
+    
+//   })
+// }
+  
---
+++ src/index.js
@@ -0,0 +1,107 @@
+'use strict'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+import NewsApiService from './fetchCountries';
+
+import countriesList from './countries-list.hbs';
+import countriesDetailList from './countries-detail-list.hbs';
+
+let debounce = require('lodash.debounce');
+
+const searchFormEl = document.getElementById('search-box');
+const listOfCountriesEl = document.querySelector('.country-list');
+
+const newsApiService = new NewsApiService();
+
+const DEBOUNCE_DELAY = 300;
+
+searchFormEl.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+
+function searchCountries(evt) {
+  evt.preventDefault();
+  
+  newsApiService.name = searchFormEl.value.trim();
+  console.log(newsApiService.name);
+
+  newsApiService.fetchCountries() 
+    .then(createMarkup)
+    .catch(onFetchError)
+}
+
+
+function onFetchError(error) {
+  console.log('CATCH');
+  listOfCountriesEl.innerHTML = '';
+  Notify.failure('Oops, there is no country with that name')
+  
+}
+
+function createMarkup(countries) {
+  console.log(countries);
+  let markup = countriesList(countries);
+  let markupDetails = countriesDetailList(countries);
+  
+
+  if (countries.length > 10) {
+    return Notify.info('Too many matches found. Please enter a more specific name.')
+  }
+ 
+  if (countries.length > 1) {
+  markup
+  }
+
+  if (countries.length === 1) {
+//     const li = [...countries].map(({ flags, name, capital, population, languages }) => {
+//             console.log(...languages.name)
+//             return ` <img src =${ flags}/><h2>${name}</h2>
+//         <ul>
+//           <li> Capital: ${capital}</li>
+//           <li> Population: ${population}</li>
+//           <li> Languages: ${languages.name}</li>
+//         </ul>
+    // ` 
+    // markup += li;
+      
+    // });
+  // 
+   markup += markupDetails;
+  }
+  return listOfCountriesEl.innerHTML = markup;
+  // return listOfCountriesEl.insertAdjacentHTML('beforeend',markup);
+  }
+
+
+// function createmarkup(countries) {
+
+//   let markup = '';
+//   console.log(...countries)
+//   console.log(countries.length)
+
+//   if (countries.length > 10) {
+//     return Notify.info('Too many matches found. Please enter a more specific name.')
+//   }
+//   if (countries.length > 1) {
+//     markup = [...countries].map(({ name, flags }) => {
+//       return `
+//    <img src =${ flags}/><h2>${name}</h2>
+//   `
+//     })
+//   }
+//   else {
+//     markup = [...countries].map(({ flags, name, capital, population, languages }) => {
+//       console.log(...languages)
+//       return ` <img src =${ flags}/><h2>${name}</h2>
+//   <ul>
+//     <li> Capital: ${capital}</li>
+//     <li> Population: ${population}</li>
+//     <li> Languages: ${languages}</li>
+//   </ul>
+//   `
+//     })
+//   }
+    
+//   console.log(markup)
+//   listOfCountriesEl.innerHTML = markup.join('');
+   
+//  }
